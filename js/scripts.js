7c5f3b321bec59074a07695587b4504243c8e8f0
Mark Helt and Nate McGregor <nate@>
2017-08-17 15:48:44 -0700

    Add more features to itterate through array

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,45 @@
 //back end
 function cryptograph(stringToSplit){
- array = stringToSplit.replace(/\W+/g, " ");
- array = array.replace(/\s/g, '');
- array = array.split("");
- alert(array);
+  var array = stringToSplit.replace(/\W+/g, " ");
+  array = array.replace(/\s/g, '');
+  array = array.split("");
 
-}
+  var col = Math.floor(Math.sqrt(array.length))
+  alert(col);
+  var arraySize = array.length;
+  var crypArray = [];
+  var indexUpdate = -1;
+  var count = 0;
+  alert(arraySize)
+
+  // for (var i = 0; i<col; ++i){
+  //   for (var i = 0; i<array.length; ++i){
+  //     if(count % col === 0){
+  //       crypArray.push(array[i])
+  //       count += 1;
+  //     }else{
+  //       count += 1;
+  //     }
+
+
+
+  for (var i = 0; i<array.length; ++i){
+    if(count % col === 0){
+      crypArray.push(array[i]);
+      count += 1;
+    }else{
+      count +=1;
+    };
+    if(count === array.length-1){
+      indexUpdate += 1;
+      if(!(indexUpdate === col)){
+        i = indexUpdate;
+        count = 0;
+      }
+    }
+  }
+  return crypArray;
+};
 
 
 //front end
